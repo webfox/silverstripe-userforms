5c9f34ed3db24ef202d48a15844b451c8a2a60ea
Otis Wright <otis@webfox.co.nz>
2016-07-06 10:19:11 +1200

    Force JS to bottom

--- code/OriginalContentExtension.php
+++ code/OriginalContentExtension.php
@@ -6,7 +6,7 @@ class OriginalContentExtension extends SiteTreeExtension
     public function contentcontrollerInit(ContentController $controller){
         Requirements::block(FRAMEWORK_DIR . '/thirdparty/jquery/jquery.js');
         Requirements::css(WF_USERFORMS_DIR . '/css/userforms.css');
-        
+        Requirements::set_force_js_to_bottom(true);
     }
 
     public function OriginalContent()
